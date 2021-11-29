557f2389de324038fa5c7090b9143d412f3f2c1f
SniperFox213 <dingolediego@gmail.com>
2021-11-29 14:25:27 +0200

    backend: Remove BullModule

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -1,5 +1,3 @@
-export * from './Queue/module';
-export * from './Cron/module';
 export * from './Subscribers/module';
 export * from './Channel/module';
 export * from './Languages/module';
--- backend/src/notifier.module.ts
+++ backend/src/notifier.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { BullModule } from '@nestjs/bull';
 import { ScheduleModule } from '@nestjs/schedule';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
@@ -11,17 +10,6 @@ import * as Modules from './modules';
     ConfigModule.forRoot(),
     ScheduleModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL),
-    BullModule.forRoot({
-      redis: {
-        host: process.env.REDIS_HOST,
-        port: Number(process.env.REDIS_PORT),
-        password: process.env.REDIS_PASSWORD,
-      },
-      defaultJobOptions: {
-        removeOnComplete: true,
-        removeOnFail: true,
-      },
-    }),
 
     // Modules
     ...Object.values(Modules),
