1c7711d62e9355ecd0d06697793bc51fa967c164
SniperFox213 <dingolediego@gmail.com>
2021-12-01 13:36:07 +0200

    storage: Test pipeline (6/-)

--- storage/src/modules/ExternalDatabase/services/E621/DatabaseFiles.service.ts
+++ storage/src/modules/ExternalDatabase/services/E621/DatabaseFiles.service.ts
@@ -1,10 +1,9 @@
 import { Injectable, Logger } from '@nestjs/common';
 
 import * as fs from 'fs';
-import * as fflate from 'fflate';
 import * as csv from 'csvtojson';
-import { unzip, deflate, unzipSync } from 'zlib';
 
+const gunzip = require('gunzip-file');
 const Downloader = require('nodejs-file-downloader');
 
 @Injectable()
@@ -25,7 +24,7 @@ export class DatabaseFilesService {
     
     // Posts download
     if (type == 'Posts') {
-      const filename = `posts-${date}`;
+      const filename = `pools-${date}`;
 
       const downloader = new Downloader({
         url: `${url}/${filename}.csv.gz`,
@@ -44,33 +43,14 @@ export class DatabaseFilesService {
         console.log(error);
       };
 
-      // Unpacking this file
-      const unzipped = fflate.gunzipSync(fs.readFileSync(`./exports/${filename}.csv.gz`));
-      this.logger.warn('File decompressed. Waiting 10 seconds and then proceeding...');
+      // Unzipping
+      this.logger.warn(`Unzipping ${filename}.csv.gz...`);
+      gunzip(`./exports/${filename}.csv.gz`, './exports/posts.csv');
 
-      await new Promise((resolve) => {
-	      setTimeout(() => { 
-		      resolve({}) 
-	      }, 10000)
-      });
+      // Parsing as csv
+      const posts = await csv().fromFile(`./exports/posts.csv`);
 
-      // Decoding
-      this.logger.warn(`Decoding file string with ${unzipped.length} length`);
-      const fileString = fflate.strFromU8(unzipped, true);
-
-      this.logger.warn(`Decoding complete. Waiting 10 seconds to parse csv...`);
-      await new Promise((resolve) => {
-	      setTimeout(() => { 
-		      resolve({}) 
-	      }, 10000)
-      });
-      fileString.slice(0, fileString.indexOf('id'));
-
-      // Parsing CSV
-      const posts = await csv()
-        .fromString(fileString);
-        
-      this.logger.warn(`Unzipped and parsed csv file. Posts length: ${posts.length}`);      
+      this.logger.warn(`Loaded ${posts.length} posts. Waiting 10 second and then starting database update process...`);
     };
   };
 };
