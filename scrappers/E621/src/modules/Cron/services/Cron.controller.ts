204c8211a979806c6a7e1eae32671126ef0a2e98
SniperFox213 <dingolediego@gmail.com>
2022-01-23 17:09:15 +0200

    scrapper/e621: Trying to fix scrapping process

--- scrappers/E621/src/modules/Cron/services/Cron.controller.ts
+++ scrappers/E621/src/modules/Cron/services/Cron.controller.ts
@@ -11,12 +11,7 @@ export class SubscriberService {
   @Cron(CronExpression.EVERY_5_SECONDS)
   public async handleCron() {
     if (!this.scrapperService.working) {
-      try {
-        await this.scrapperService.process();
-      } catch(error) {
-        console.log('error while scrapping:');
-        console.log(error);
-      };
+      await this.scrapperService.process();
     };
   };
 };
--- scrappers/E621/src/modules/Scrapper/services/Scrapper.service.ts
+++ scrappers/E621/src/modules/Scrapper/services/Scrapper.service.ts
@@ -22,38 +22,41 @@ export class ScrapperService {
     // Changing scrapper status
     this.working = true;
 
-    console.log('processing new posts');
+    try {
+      let latestPost: IPost = await this.storageService.getLatestPost();
 
-    let latestPost: IPost = await this.storageService.getLatestPost();
+      // Checking if we have latest post information
+      // or no
+      if (!latestPost?.id) {
+        // Getting latest post from E621
+        latestPost = await this.postsService.fetchOne([]);
+        
+        // Updating information about latest post in storage
+        await this.storageService.updateLatestPost(latestPost);
 
-    // Checking if we have latest post information
-    // or no
-    if (!latestPost?.id) {
-      // Getting latest post from E621
-      latestPost = await this.postsService.fetchOne([]);
-      
-      // Updating information about latest post in storage
-      await this.storageService.updateLatestPost(latestPost);
+        // Waiting 5 seconds to not violate E621 request limit
+        await (new Promise((resolve) => setTimeout(() => resolve(null), 5000)));
+      };
 
-      // Waiting 5 seconds to not violate E621 request limit
-      await (new Promise((resolve) => setTimeout(() => resolve(null), 5000)));
-    };
+      // Getting new posts
+      const posts = await this.postsService.fetchMany([], 10, { page: `a${latestPost.id}` });
 
-    // Getting new posts
-    const posts = await this.postsService.fetchMany([], 10, { page: `a${latestPost.id}` });
+      if (posts.length > 0) {
+        console.log("process new posts");
+        console.log(posts.length);
+        // +todo
+        // add new posts to storage instance      
 
-    if (posts.length > 0) {
-      console.log("process new posts");
-      console.log(posts.length);
-      // +todo
-      // add new posts to storage instance      
+        // Ask backend instance to process
+        // new posts
+        this.backendService.processNewPosts(posts);
 
-      // Ask backend instance to process
-      // new posts
-      this.backendService.processNewPosts(posts);
-
-      // Update latestPost in storage database
-      this.storageService.updateLatestPost(posts[0]);
+        // Update latestPost in storage database
+        this.storageService.updateLatestPost(posts[0]);
+      };
+    } catch(error) {
+      console.log("error in scrapper process:");
+      console.log(error);
     };
 
     // Updating scrapper status
