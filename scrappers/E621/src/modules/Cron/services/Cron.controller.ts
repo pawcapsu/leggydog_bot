ad56c5748e71c56f8a25b840faf1e0c2e4a84e54
SniperFox213 <dingolediego@gmail.com>
2022-06-11 17:29:23 +0300

    scrapper/e621: Add logging

--- scrappers/E621/src/modules/Cron/services/Cron.controller.ts
+++ scrappers/E621/src/modules/Cron/services/Cron.controller.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { ScrapperService } from 'src/modules/Scrapper/services';
 
@@ -8,8 +8,11 @@ export class SubscriberService {
     private readonly scrapperService: ScrapperService,
   ) {}
 
+  private readonly logger = new Logger(SubscriberService.name);
+
   @Cron(CronExpression.EVERY_5_SECONDS)
   public async handleCron() {
+    this.logger.debug(`Handle cron (isWorking: ${this.scrapperService.working})`);
     if (!this.scrapperService.working) {
       await this.scrapperService.process();
     };
--- scrappers/E621/src/modules/Scrapper/services/Scrapper.service.ts
+++ scrappers/E621/src/modules/Scrapper/services/Scrapper.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { BackendService } from 'src/modules/Backend/services';
 import { PostsService } from 'src/modules/E621/services';
 import { StorageService } from 'src/modules/Storage/services';
@@ -13,26 +13,35 @@ export class ScrapperService {
     private readonly postsService: PostsService,
   ) {}
 
+  private readonly logger = new Logger(ScrapperService.name);
+  
   // Scrapper status
   public working: boolean = false;
 
   // public process
   // - Start scrapping process
   public async process() {
+    this.logger.debug('Start scrapping process');
+
     // Changing scrapper status
     this.working = true;
 
     try {
       let latestPost: IPost = await this.storageService.getLatestPost();
+      this.logger.debug('Got latest post from Storage Service:', latestPost);
 
       // Checking if we have latest post information
       // or no
       if (!latestPost?.id) {
+        this.logger.warn('Latest post not found in Storage Service.');
+
         // Getting latest post from E621
         latestPost = await this.postsService.fetchOne([]);
         
         // Updating information about latest post in storage
         await this.storageService.updateLatestPost(latestPost);
+
+        this.logger.debug('New latest post:', latestPost);
 
         // Waiting 5 seconds to not violate E621 request limit
         await (new Promise((resolve) => setTimeout(() => resolve(null), 5000)));
@@ -41,25 +50,27 @@ export class ScrapperService {
       // Getting new posts
       const posts = await this.postsService.fetchMany([], 10, { page: `a${latestPost.id}` });
 
+      this.logger.debug('Got new posts from Posts Service:', posts);
+
       if (posts.length > 0) {
-        console.log("process new posts");
-        console.log(posts.length);
         // +todo
         // add new posts to storage instance      
-
+        
         // Ask backend instance to process
         // new posts
+        this.logger.debug('Asking Backend Service to process new posts');
         this.backendService.processNewPosts(posts);
 
         // Update latestPost in storage database
+        this.logger.debug('Asking Storage Service to store last post');
         this.storageService.updateLatestPost(posts[0]);
       };
     } catch(error) {
-      console.log("error in scrapper process:");
-      console.log(error);
+      this.logger.warn('Error in scrapper process:', error);
     };
 
     // Updating scrapper status
     this.working = false;
+    this.logger.debug('Scrapping process end.');
   };
 };
